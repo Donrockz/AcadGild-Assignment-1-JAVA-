3f408a3638c0dd7b4b98cc699ac6d5c92dffd055
Donrockz <rockstarrockey18@gmail.com>
2017-05-31 21:42:34 +0530

    Add files via upload

---
+++ Assignment1.1/src/assignment1/Ass.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+public class Ass {
+
+	public static void main(String[] args) {
+		
+		int a = 12;
+		int b = 7;
+		int c = a - (-b);
+				
+		
+		System.out.println("The result is " + c);
+
+	}
+	
+}
+
---
+++ Assignment1.1/src/assignment1/Ass12.java
@@ -0,0 +1,29 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class Ass12 {
+
+	public static void main(String[] args) {
+		
+		Scanner Num = new Scanner(System.in);
+		
+		System.out.println("Enter the First Number?");
+		
+		String FN = Num.next();
+		
+		Scanner No = new Scanner(System.in);
+		
+		System.out.println("Enter the Second Number?");
+		String SN = No.next();
+		
+		System.out.println("The result of addition is " + FN-(-SN));
+		
+		
+		
+		
+		
+		
+			
+	}
+}
